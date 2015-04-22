a82e7b05e7edad1b006e6800218d1ebcff12dbaf
Daniel Mierswa <daniel.b.mierswa@student.hs-rm.de>
2015-04-22 10:00:59 +0200

    initial README and hello world project

---
+++ helloworld/src/main.c
@@ -0,0 +1,15 @@
+#include <autoconf.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main(void)
+{
+	printf("Switching to a safer, bigger stack... ");
+	fflush(stdout);
+
+	return 0;
+}
